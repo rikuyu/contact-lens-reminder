4c94deffc44adc6b45e7f257beec7cd49377635d
rikuyu <rikuyu5884@gmail.com>
2022-01-31 20:58:00 +0900

    MaterialDialog, 問い合わせ画面のKeyboard Scroll, roundIcon

--- app/src/main/java/io/github/rikuyu/contactlensreminder/presentation/screens/lens_setting/components/SetNotificationTimeSection.kt
+++ app/src/main/java/io/github/rikuyu/contactlensreminder/presentation/screens/lens_setting/components/SetNotificationTimeSection.kt
@@ -6,23 +6,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.chargemap.compose.numberpicker.NumberPicker
+import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 import io.github.rikuyu.contactlensreminder.R
 import io.github.rikuyu.contactlensreminder.presentation.theme.CleanBlue
 import io.github.rikuyu.contactlensreminder.presentation.theme.PaleBlue
 import io.github.rikuyu.contactlensreminder.presentation.util.SimpleDivider
-import io.github.rikuyu.contactlensreminder.presentation.util.StringNumberPicker
 
 @Composable
 fun SetNotificationTimeSection(
@@ -34,7 +35,7 @@ fun SetNotificationTimeSection(
     setNotificationTimeHour: (Int) -> Unit,
     setNotificationTimeMinute: (Int) -> Unit
 ) {
-    var isShowLensPeriodPicker by remember { mutableStateOf(false) }
+    val dialogState = rememberMaterialDialogState(false)
 
     Column(
         modifier = modifier.background(Color.White)
@@ -52,50 +53,39 @@ fun SetNotificationTimeSection(
                     .padding(start = 10.dp)
                     .background(Color.White)
             )
-            if (isShowLensPeriodPicker) {
-                NumberPicker(
-                    value = notificationTimeHour,
-                    onValueChange = { setNotificationTimeHour(it) },
-                    range = 1..24,
-                    textStyle = LocalTextStyle.current.copy(color = Color.Black)
-                )
-                Text(text = " : ")
-                StringNumberPicker(
-                    value = notificationTimeMinute.toString(),
-                    onValueChange = { setNotificationTimeMinute(it.toInt()) },
-                    range = listOf("00", "15", "30", "45"),
-                    textStyle = LocalTextStyle.current.copy(color = Color.Black)
-                )
-            } else {
-                Box(
-                    modifier = Modifier
-                        .background(PaleBlue, shape = RoundedCornerShape(20))
-                        .padding(vertical = 8.dp, horizontal = 10.dp)
-                ) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Text(
-                            text = notificationTimeHour.toString(),
-                            color = textColor,
-                            fontSize = 20.sp
-                        )
-                        Text(
-                            text = stringResource(id = R.string.time_div),
-                            color = textColor,
-                            fontSize = fontSize,
-                            modifier = Modifier.padding(horizontal = 4.dp)
-                        )
-                        Text(
-                            text = if (notificationTimeMinute == 0)
-                                "0$notificationTimeMinute"
-                            else notificationTimeMinute.toString(),
-                            color = textColor,
-                            fontSize = 20.sp
-                        )
-                    }
+            Box(
+                modifier = Modifier
+                    .background(PaleBlue, shape = RoundedCornerShape(20))
+                    .padding(vertical = 8.dp, horizontal = 10.dp)
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = notificationTimeHour.toString(),
+                        color = textColor,
+                        fontSize = 20.sp
+                    )
+                    Text(
+                        text = stringResource(id = R.string.time_div),
+                        color = textColor,
+                        fontSize = fontSize,
+                        modifier = Modifier.padding(horizontal = 4.dp)
+                    )
+                    Text(
+                        text = if (notificationTimeMinute == 0)
+                            "0$notificationTimeMinute"
+                        else notificationTimeMinute.toString(),
+                        color = textColor,
+                        fontSize = 20.sp
+                    )
                 }
             }
+            NotificationTimeDialog(dialogState = dialogState, setNotificationTime = {
+                val (hour, min) = it.split(":").map(String::toInt)
+                setNotificationTimeHour(hour)
+                setNotificationTimeMinute(min)
+            })
             Button(
-                onClick = { isShowLensPeriodPicker = !isShowLensPeriodPicker },
+                onClick = { dialogState.showing = true },
                 colors = ButtonDefaults.textButtonColors(
                     backgroundColor = CleanBlue,
                     contentColor = Color.White,
@@ -104,18 +94,11 @@ fun SetNotificationTimeSection(
                 modifier = Modifier.padding(start = 16.dp),
                 shape = RoundedCornerShape(20)
             ) {
-                if (isShowLensPeriodPicker) {
-                    Text(
-                        text = stringResource(id = R.string.ok),
-                        fontSize = fontSize
-                    )
-                } else {
-                    Icon(
-                        imageVector = Icons.Default.Refresh,
-                        contentDescription = null,
-                        tint = Color.White
-                    )
-                }
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_time),
+                    contentDescription = null,
+                    tint = Color.White
+                )
             }
         }
         SimpleDivider()
