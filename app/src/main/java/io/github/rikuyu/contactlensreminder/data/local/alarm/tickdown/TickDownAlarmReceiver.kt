bec1bef771a8a743101bf6a52e53c928fe7e747a
rikuyu <rikuyu5884@gmail.com>
2022-02-03 01:09:27 +0900

    error 解消

--- app/src/main/java/io/github/rikuyu/contactlensreminder/data/local/alarm/tickdown/TickDownAlarmReceiver.kt
+++ app/src/main/java/io/github/rikuyu/contactlensreminder/data/local/alarm/tickdown/TickDownAlarmReceiver.kt
@@ -7,14 +7,14 @@ import io.github.rikuyu.contactlensreminder.data.local.sharedpreferences.SharedP
 import io.github.rikuyu.contactlensreminder.data.util.ChangeAppIconService
 import javax.inject.Inject
 
-class TickDownAlarmReceiver @Inject constructor(
-    private val sharedPreferencesManager: SharedPreferencesManager,
-    private val changeAppIconService: ChangeAppIconService,
-    private val tickDownAlarmManager: TickDownAlarmManager
-) : BroadcastReceiver() {
+class TickDownAlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         context?.let {
+            val sharedPreferencesManager = SharedPreferencesManager(it)
+            val changeAppIconService = ChangeAppIconService(it)
+            val tickDownAlarmManager = TickDownAlarmManager(it)
+
             val remainingDay = sharedPreferencesManager.getContactLensRemainingDays()
             val isUsingContactLens = sharedPreferencesManager.getIsUsingContactLens()
             if (remainingDay > 0) {
