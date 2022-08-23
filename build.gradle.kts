38809877864bda5bf852e69bd0fc5dfd9d621578
rikuyu <rikuyu5884@gmail.com>
2022-08-24 00:45:14 +0900

    improve build setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
     dependencies {
         classpath("com.android.tools.build:gradle:7.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
@@ -6,12 +17,6 @@ buildscript {
         classpath("com.google.gms:google-services:4.3.13")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.1")
     }
-}
-
-plugins {
-    id("com.android.application") version "7.1.0" apply false
-    id("com.android.library") version "7.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.5.21" apply false
 }
 
 tasks.register("clean", Delete::class) {
